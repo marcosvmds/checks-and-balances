b5016a11f3dd41dd5e0013e34648f7b43d86421f
MarcosMazzei <mazzeimarcosv@gmail.com>
2021-04-29 10:46:21 -0300

    base URL issue (hardcoding url)

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL = process.env.API_URL || "localhost:3001"
 
 const api = axios.create({
-    baseURL
+    baseURL: "https://checks-and-balances.herokuapp.com/"
 })
 
 console.log(baseURL)
